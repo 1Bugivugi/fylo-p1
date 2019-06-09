47554f5984ef81117ce10496befee4ba8cc2d595
Gleb Rezoun <gleb.rezun@gmail.com>
2019-06-10 01:49:28 +0300

    Updates

---
+++ precache-manifest.0ea0fdee74d9a3eaca3616a032cfcadb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a30e57cdfcc5b82d1bcae057e03b1d7d",
+    "url": "/fylo-p1/index.html"
+  },
+  {
+    "revision": "9c53be032340c409fac2",
+    "url": "/fylo-p1/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "344f7b8e040b8d811c2b",
+    "url": "/fylo-p1/static/css/main.71052072.chunk.css"
+  },
+  {
+    "revision": "9c53be032340c409fac2",
+    "url": "/fylo-p1/static/js/2.34ea9862.chunk.js"
+  },
+  {
+    "revision": "344f7b8e040b8d811c2b",
+    "url": "/fylo-p1/static/js/main.1b11f6e2.chunk.js"
+  },
+  {
+    "revision": "8d7e8e6a2acaf64eda72",
+    "url": "/fylo-p1/static/js/runtime~main.7d4bbbca.js"
+  },
+  {
+    "revision": "6d87f67460b1c8f2ea42b19cfb14821c",
+    "url": "/fylo-p1/static/media/bg-intro.6d87f674.svg"
+  },
+  {
+    "revision": "2d07e600de0d5aa4db39972ac8b66306",
+    "url": "/fylo-p1/static/media/illustration-main.2d07e600.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fylo-p1/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ee1545b52539f5ea6e2f787fd921f468",
+    "url": "/fylo-p1/static/media/logo.ee1545b5.svg"
+  }
+]);
